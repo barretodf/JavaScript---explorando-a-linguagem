f06986e1bb9744532b2c9f45740a4a2e9b89f791
barretodf <edsb1981@gmail.com>
2023-04-01 22:34:05 -0300

    Conversões de tipos

---
+++ conversoesDeTipos.js
@@ -0,0 +1,12 @@
+console.log('2' + '2');
+console.log('2' * '2');
+console.log(parseInt ('2') + parseInt ('2'));
+
+console.log(parseInt ('5') + 5);
+
+//No caso de soma tem que usar o parseInt para converter
+//No caso de divisão ele faz automáticamente
+//Se dividir um nome por um número vai aparecer no terminal NaN = Not a Number
+
+console.log(6.5);
+console.log(6,5);
