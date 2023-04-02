778d97faad65f428e86cfd1506556cd402f431e6
barretodf <edsb1981@gmail.com>
2023-04-01 23:04:55 -0300

    Criando listas

---
+++ listas.js
@@ -0,0 +1,13 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDeDestinos = new Array (
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+
+);
+console.log('Destinos possíveis');
+console.log(listaDeDestinos);
