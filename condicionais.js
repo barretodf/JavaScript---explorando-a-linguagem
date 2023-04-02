fb31721eca1eb6edbc4f3c376684c7002eaaa4ae
barretodf <edsb1981@gmail.com>
2023-04-02 00:21:49 -0300

    IFs encadeados

--- condicionais.js
+++ condicionais.js
@@ -1,22 +1,28 @@
 console.log(`Trabalhando com condicionais`);
 
-const listaDeDestinos = new Array (
+const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
     `Rio de Janeiro`
 );
-const idadeComprador = 17
+const idadeComprador = 17;
+const acompanhadaPorAdulto = true;
 
 console.log('Destinos possíveis');
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
-    
+if (idadeComprador >= 18) {
+
     console.log('Comprador maior de idade');
-    listaDeDestinos.splice(1,1) //O primeiro 1 é o ítem, o segundo 1 é a quantidade de ítens
-}else{
+    listaDeDestinos.splice(1, 1) //O primeiro 1 é o ítem, o segundo 1 é a quantidade de ítens
+} else if (acompanhadaPorAdulto) {
+    console.log('Comprador acompanhado por pessoa maior de idade');
+    listaDeDestinos.splice(1, 1)
+
+} else {
     console.log('Proibida a venda para menores de idade!')
 }
+
 
 
 //Splice deleta ítens da lista
@@ -26,8 +32,8 @@ console.log(listaDeDestinos);
 
 
 //Operadores lógicos
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador == 18);
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador == 18);
