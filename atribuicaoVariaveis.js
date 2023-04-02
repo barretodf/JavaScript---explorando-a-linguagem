9521336bd8207fe32db6ee721b5d77fbd6853861
barretodf <edsb1981@gmail.com>
2023-04-01 22:45:44 -0300

    atribuição de variáveis

---
+++ atribuicaoVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const nome = "Edmilson";
+const sobrenome = "Barreto"; 
+const nomeCompleto = (nome, sobrenome);// imprime só o sobrenome
+
+console.log(nome + sobrenome);//Nomes concatenados
+
+console.log(nome, sobrenome);//Nomes separados
+
+console.log(`Meu nome é ${nome} ${sobrenome}`); //Nomes concatenados 
+
+console.log(nomeCompleto); //Só o sobrenome
