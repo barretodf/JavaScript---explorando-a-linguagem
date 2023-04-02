6e7553aef60db7c0c50fbb9e468afef46e9d9b64
barretodf <edsb1981@gmail.com>
2023-04-02 00:05:38 -0300

    Operadores lógicos

--- condicionais.js
+++ condicionais.js
@@ -5,7 +5,8 @@ const listaDeDestinos = new Array (
     `São Paulo`,
     `Rio de Janeiro`
 );
-const idadeComprador = 18
+const idadeComprador = 17
+
 console.log('Destinos possíveis');
 console.log(listaDeDestinos);
 
@@ -13,9 +14,20 @@ if(idadeComprador >= 18){
     
     console.log('Comprador maior de idade');
     listaDeDestinos.splice(1,1) //O primeiro 1 é o ítem, o segundo 1 é a quantidade de ítens
+}else{
+    console.log('Proibida a venda para menores de idade!')
 }
+
 
 //Splice deleta ítens da lista
 
 
 console.log(listaDeDestinos);
+
+
+//Operadores lógicos
+console.log(idadeComprador > 18);
+console.log(idadeComprador < 18);
+console.log(idadeComprador >= 18);
+console.log(idadeComprador <= 18);
+console.log(idadeComprador == 18);
